b9104240facaff9bd532e4affbb7b58ecf310e9e
klosorio10 <kl.osorio10@uniandes.edu.co>
2017-03-30 07:07:47 -0500

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -9,6 +9,7 @@ class Home extends Component {
                 <div className="col-md-2"></div>
                 <div className="col-md-10">
                     <h1 className="text-vertical-center">Welcome to GanaSoft</h1>
+                <!-- debria mostrar un poco más de dinamicas si es la paguina principal, como descripciones de lo que puede hacer el usuario -->
                     <h2> GanaSoft is a Web Page where you can administrate your farms </h2>
                 </div>
 
